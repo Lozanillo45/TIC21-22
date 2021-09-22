b2b568025a9ed99c20c613e1a5d312729ab581d6
Lozanillo45 <90762312+Lozanillo45@users.noreply.github.com>
2021-09-22 11:38:54 +0200

    Add files via upload

---
+++ saludador2.py
@@ -0,0 +1,10 @@
+def saludador2():
+    nombre = raw_input("¿Como te llamas?")
+    edad=input("¿Cuantos años tienes?")
+    lugar=raw_input("¿Donde vives?")
+    print("Buenos dias, "+nombre)
+    print("tienes "+str(edad)+" años")
+    print("y se que vives en "+lugar)
+    print("ten cuidado")
+
+saludador2()
