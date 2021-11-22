bb7ee70ebd7acfc5b74dfd1fc16238687e5fd6f5
Lozanillo45 <90762312+Lozanillo45@users.noreply.github.com>
2021-11-22 10:00:27 +0100

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,29 @@
+import random
+
+def dados():
+    """Introduzca el nombre de los jugadores
+    PARTIDAS
+    Turno 1
+    Jugador 1
+    Tirada 1=6
+    Jugador 2
+    Tirada 1=3
+    [...]
+    RESULTADOS
+    Puntos jugador 1=
+    Puntos jugador 2=
+    Ha ganado jugador 2"""
+    print("Introduce el nombre de los jugadores")
+    player1=raw_input("PLAYER ONE: ")
+    player2=raw_input("PLAYER TWO: ")
+    puntos_1=0
+    puntos_2=0
+    while():
+        print("TURNO 1")
+        tirada=random.randint()
+        print("Tirada 1 "+player1+" = "+tirada)
+        puntos_1=puntos_1+tirada
+
+
+
+dados()
