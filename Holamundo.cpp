1ba443d79463a4b7d59f3d50053b635d1e1e8f68
Lozanillo45 <90762312+Lozanillo45@users.noreply.github.com>
2021-12-01 12:01:43 +0100

    Add files via upload

---
+++ Holamundo.cpp
@@ -0,0 +1,16 @@
+//Esto es un comentario guachines
+/*Esto es un comentario de
+más líneas*/
+//INSTRUCCIONES AL PREPROCESADOR
+#include<stdio.h>//FICHERO DE CABECERA
+int main(){
+	//DECLARACIÓN DE VARIABLES
+	char salir;
+	printf("Hola mundo, cruel");
+	printf("\nPRESIONE CUALQUIER TECLAS PARA SALIR: ");
+	scanf("%c",&salir);
+	return 0;
+	
+	
+	
+}
