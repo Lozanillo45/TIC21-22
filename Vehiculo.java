0c24cc5b0414edf21a8a58f55e6cf9749afc8cd8
Lozanillo45 <90762312+Lozanillo45@users.noreply.github.com>
2022-03-23 11:41:59 +0100

    Add files via upload

---
+++ Vehiculo.java
@@ -0,0 +1,54 @@
+package MiCodigo;
+
+public class Vehiculo {
+	private String identificador;
+	private String medio;
+	private double velocidad;
+	private int capacidad;
+	private int ruedas;
+	private int puertas;
+	
+	public Vehiculo(String identificador, String medio) {
+		super();
+		this.identificador = identificador;
+		this.medio = medio;
+	}
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	public String getMedio() {
+		return medio;
+	}
+	public void setMedio(String medio) {
+		this.medio = medio;
+	}
+	public double getVelocidad() {
+		return velocidad;
+	}
+	public void setVelocidad(double velocidad) {
+		this.velocidad = velocidad;
+	}
+	public int getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+	public int getRuedas() {
+		return ruedas;
+	}
+	public void setRuedas(int ruedas) {
+		this.ruedas = ruedas;
+	}
+	public int getPuertas() {
+		return puertas;
+	}
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+	
+	
+}
